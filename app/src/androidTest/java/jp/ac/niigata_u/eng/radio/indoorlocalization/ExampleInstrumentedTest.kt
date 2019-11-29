bf63bb91f6b801d9043ebd5a55ce6555cbf9b770
saccho <saccho956@yahoo.co.jp>
2019-11-30 01:26:38 +0900

    [clean] reformat code for all files

--- app/src/androidTest/java/jp/ac/niigata_u/eng/radio/indoorlocalization/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/jp/ac/niigata_u/eng/radio/indoorlocalization/ExampleInstrumentedTest.kt
@@ -15,10 +15,10 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("jp.ac.niigata_u.eng.radio.indoorlocalization", appContext.packageName)
-    }
+  @Test
+  fun useAppContext() {
+    // Context of the app under test.
+    val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+    assertEquals("jp.ac.niigata_u.eng.radio.indoorlocalization", appContext.packageName)
+  }
 }

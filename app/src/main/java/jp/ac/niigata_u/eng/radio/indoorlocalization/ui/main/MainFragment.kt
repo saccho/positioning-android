6267d0d2467fc78fba0ce08a66230f942c4a0402
saccho <saccho956@yahoo.co.jp>
2019-12-10 13:09:37 +0900

    Fix to hide soft keyboard at screen transition

--- app/src/main/java/jp/ac/niigata_u/eng/radio/indoorlocalization/ui/main/MainFragment.kt
+++ app/src/main/java/jp/ac/niigata_u/eng/radio/indoorlocalization/ui/main/MainFragment.kt
@@ -1,9 +1,11 @@
 package jp.ac.niigata_u.eng.radio.indoorlocalization.ui.main
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import jp.ac.niigata_u.eng.radio.indoorlocalization.R
@@ -21,6 +23,8 @@ class MainFragment : Fragment() {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     connect_button.setOnClickListener {
+      val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+      imm.hideSoftInputFromWindow(view.windowToken, 0)
       val ipAddress = ip.text.toString()
       val portNo = port.text.toString().toInt()
       findNavController().navigate(
